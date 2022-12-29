818a768786403229fd7ae3d08c5e38623b3b706e
Alexande <79607025659@yandex.ru>
2022-12-29 13:22:46 +0300

    26

---
+++ семинар 4.C#/task26/Program.cs
@@ -0,0 +1,22 @@
+﻿/*лександр Токарев: Задача 26: 
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5  */
+
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (number>0)
+{
+    number/=10;
+    count ++;
+}
+Console.WriteLine("total: {0} ", count);  
+
+/* решение через строки
+Console.WriteLine("введите число");
+string a =  Console.ReadLine();
+int b = a.Length;
+Console.WriteLine("total "+b+" ");*/
