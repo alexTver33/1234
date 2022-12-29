25ee65baa8186f0c9c1658a87d6580d8cb235fc0
Alexande <79607025659@yandex.ru>
2022-12-29 13:22:20 +0300

    28

---
+++ семинар 4.C#/task28/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Александр Токарев: Решение в группах задач:
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 */
+
+
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int Proizv (int M)
+{
+int pz =1;
+while(1!=M)
+{
+    pz = pz * M;
+    M--;
+}return pz;
+}
+Console.WriteLine(Proizv(N));
